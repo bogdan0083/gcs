56c7e45e8b2686170acbbcb123985d1ab5dcb7eb
Bogdan <d.bogdan@inbox.ru>
2017-02-09 14:18:01 +0700

    finish landing

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
-    // trigger navigation on burger menu
+    // Код для мобильного меню:
+    
     var $navTrigger = $('.nav-trigger');
     var $mainNav = $('.nav-main');
     var $navLinks = $mainNav.find('a');
 
+    // обработчик иконки меню:
+    
     $navTrigger.click(function(e) {
         // body...
         e.preventDefault();
@@ -12,6 +15,8 @@ $(document).ready(function() {
         $mainNav.slideToggle();
     });
 
+    // Подсвечивание активного пункта меню на ховер
+    // 
     $mainNav.on('mouseover', 'a', function(e) {
         // body...
         e.preventDefault();
@@ -26,6 +31,9 @@ $(document).ready(function() {
         $(this).removeClass('js-transparent');
     });
 
+    // ----------------------------------------------
+    // 
+    // 
     // Portfolio filter
     // 
     $('.projects-gallery').imagesLoaded(function() {
@@ -41,9 +49,12 @@ $(document).ready(function() {
             }
         });
     });
-
+    // ---------------------------------------------
+    // 
+    // 
+    // 
     // init animations on scroll (WOW.JS)
-
+    //
     var wow = new WOW({
         mobile: false,
         callback: function(box) {
@@ -54,6 +65,7 @@ $(document).ready(function() {
             	$(box).find('.globe-big').addClass('animated zoomIn');
             };
 
+            // Анимация чисел
             var $counter = $(box).find('.js-counter');
 
         	if ($counter.length > 0) {
@@ -70,11 +82,10 @@ $(document).ready(function() {
         }
 
     });
-
     wow.init();
-
     // ----------------------------------
-
+    //
+    //
     // remove default event on click;
 
     var $filterLinks = $('.projects-filter a');
@@ -87,7 +98,9 @@ $(document).ready(function() {
     });
 
     // --------------------------------
-
+    //
+    //
+    // Слайдер в новостях
     $('.news-item-slider').slick({
         arrows: false,
         dots: false
@@ -99,7 +112,11 @@ $(document).ready(function() {
         console.log($(this).closest('.news-item-slider'));
         $(this).closest('.news-item-slider').slick('slickNext');
     });
-
+    // ------------------------------------------------------
+    // 
+    // 
+    // google карта
+    // 
     var map;
 
     var windowWidth = $('window').width();
@@ -137,22 +154,25 @@ $(document).ready(function() {
             position: myLatLng,
             map: map,
             title: 'Hello World!',
-            icon: '../img/marker.png'
+            icon: '/img/marker.png'
         });
     }
 
     initMap();
-
-
-    // event POPUP custom scrollbar
-
+    // -----------------------------------
+    // 
+    // 
+    // Кастомный скроллбар в попапе
+    // Если попап будет подгружаться аяксом,
+    // Надо его переинициализировать
+    //
     $('.gcs-popup-inner').scrollbar();
-
     // ---------------------------
-
+    //
+    //
+    //
     // arrow UP!
     // 
-
     var $arrowUp = $('.go-top');
 
     $arrowUp.click(function(e) {
@@ -162,14 +182,35 @@ $(document).ready(function() {
             'scrollTop': 0
         });
     });
+    // -----------------------------------
+    // 
+    // 
+    // Слайдер в попапе. Инициализируем через each
+    // для правильной работы стрелок
+    // 
+    $('.gcs-popup-slider').each(function() {
+        // body...
+        
+        var _this = $(this);
+        var $wrapper = _this.find('.wrapper');
 
-    $('.gcs-popup-slider .wrapper').slick({
-        arrows: true,
-        infinite: false,
-        prevArrow: $('.arr-slider-left'),
-        nextArrow: $('.arr-slider-right'),
+        $wrapper.slick({
+            arrows: true,
+            infinite: false,
+            prevArrow: _this.find('.arr-slider-left'),
+            nextArrow: _this.find('.arr-slider-right'),
+        });
+
     });
-
+    // ----------------------------------------
+    // 
+    // 
+    // Инициализация попапа
+    // В HTML я добавил в самом низу несколько блоков .gcs-popup для наглядности.
+    // Если попапы будут подгружаться аяксом, нужно почитать доку:
+    // http://dimsemenov.com/plugins/magnific-popup/documentation.html#ajax-type
+    // Или написать мне ;)
+    // 
     var currentPopupIndex;
     // Initialize popup as usual
     $('.popup-link').magnificPopup({
@@ -191,7 +232,14 @@ $(document).ready(function() {
         e.preventDefault();
         $.magnificPopup.close();
     });
-
+    // ----------------------------------------
+    // 
+    // 
+    // 
+    // Реализация стрелок в попапе. 
+    // Просто листаем попап по индексу.
+    // Если через аякс, можно вызывать нужный попап через метод .open()
+    // 
     $('.arr-popup-prev').click(function(e) {
         e.preventDefault();
         $.magnificPopup.close();
@@ -213,7 +261,12 @@ $(document).ready(function() {
             $('.popup-link').magnificPopup('open', currentPopupIndex + 1);
         }, 300);
     });
-
+    // ----------------------------
+    // 
+    // 
+    // 
+    // Навигация
+    // 
     var $anchors = $('.js-anchor');
 
     $anchors.click(function(e) {
@@ -226,7 +279,12 @@ $(document).ready(function() {
             'scrollTop': $(id).offset().top - $('.header').height()
         });
     });
-
+    // ----------------------------------
+    // 
+    // 
+    // 
+    // Окрашивание шапки при скролле
+    // 
     if ($(window).width() > 880) {
 
         var headerPainted = null;
@@ -251,4 +309,5 @@ $(document).ready(function() {
 
         paintHeader();
     }
+    // END -----------------------------------
 });
